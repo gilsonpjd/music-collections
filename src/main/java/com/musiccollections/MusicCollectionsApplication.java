84f5fec017db33a9042a9b2c3644cb2b755574f8
José Junior <gilson.pjd@gmail.com>
2023-10-22 11:40:20 +0100

    first commit

---
+++ src/main/java/com/musiccollections/MusicCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.musiccollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musiccollections/MusicCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musiccollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
