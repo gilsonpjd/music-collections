707ab3316bc5a8cd07b8c4c21421666cad81cea8
José Junior <gilson.pjd@gmail.com>
2023-11-01 08:07:31 +0000

    API Using OpenAPI 3.0 added and working correctly

--- src/main/java/com/musiccollections/MusicCollectionsApplication.java
+++ src/main/java/com/musiccollections/MusicCollectionsApplication.java
@@ -1,28 +1,12 @@
 package com.musiccollections;
 
-import com.musiccollections.database.DatabaseConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLException;
-
 @SpringBootApplication
 public class MusicCollectionsApplication {
-    private String url;
-
-    private String userName;
-
-    private String password;
-
     public static void main(String[] args) {
         SpringApplication.run(MusicCollectionsApplication.class, args);
     }
 
-    public void initializeDatabaseConnector() {
-        try {
-            DatabaseConnector databaseConnector = new DatabaseConnector(url, userName, password);
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
